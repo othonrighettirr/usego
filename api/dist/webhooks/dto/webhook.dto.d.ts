6f722d46a0e76c17c55668e290025d3eea5383dc
GO API <61289623+usegoapi@users.noreply.github.com>
2026-01-11 11:32:40 -0300

    Delete api directory

--- api/dist/webhooks/dto/webhook.dto.d.ts
+++
@@ -1,9 +0,0 @@
-export declare class CreateWebhookDto {
-    url: string;
-    events: string[];
-}
-export declare class UpdateWebhookDto {
-    url?: string;
-    events?: string[];
-    active?: boolean;
-}
